dc794a6a2dd26fd7e49f66dd4fa88598e8756964
unknown <Administrator@DESKTOP-EE59TLO.wrldc.local>
2019-06-20 16:07:32 +0530

    20-06-2019

--- app/frequecny_profile.py
+++ app/frequecny_profile.py
@@ -1,5 +1,18 @@
+from openpyxl import Workbook
+from openpyxl import load_workbook
+import pandas as pd 
 
-# frequency_xcel_data = pd.ExcelFile('../Files/Frequency.xlsx')
-frequency_xcel_data = open('../Files/Frequency.xlsx','r')
+wb =load_workbook(filename = '../Files/Frequency.xlsx')
+ws = wb.active
 
-print(frequency_xcel_data.read())
+frequency_col_range = ws['B']
+frequency_row_range = ws[2:81]
+frequency_value = []
+
+for row in frequency_row_range:
+    for col in frequency_col_range:
+        print(col.value)
+
+# df = pd.DataFrame(frequency_value)
+
+# print(max(frequency_value))
