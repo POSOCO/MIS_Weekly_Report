bd635909f6fdea026c55b5857b52f0e49020f8dd
unknown <Administrator@DESKTOP-EE59TLO.wrldc.local>
2019-06-20 16:56:25 +0530

    Band Duration

--- app/frequecny_profile.py
+++ app/frequecny_profile.py
@@ -2,6 +2,10 @@ from openpyxl import Workbook
 from openpyxl import load_workbook
 import pandas as pd 
 
+def avg_frequency(lst):
+    return (sum(lst)/len(lst))
+
+
 wb =load_workbook(filename = '../Files/Frequency.xlsx')
 ws = wb.active
 
@@ -14,13 +18,31 @@ for row in frequency_cell:
         frequency_value.append(col.value)
 
 
-print(max(frequency_value))
+max_frequency = round(max(frequency_value),2)
 
-print(min(frequency_value))
+min_frequency = round(min(frequency_value),2)
 
-
-def avg_frequency(lst):
-    return (sum(lst)/len(lst))
+avg_frequency = round(avg_frequency(frequency_value),2) 
 
 
-print(round(avg_frequency(frequency_value),2))
+def band_duration_info():
+    below_band = 0
+    in_the_band = 0
+    above_band = 0
+    percent_value = 86.40
+    for each in frequency_value:
+        if each < 49.90:
+            below_band += 1
+        elif each >= 49.90 and each <= 50.05:
+            in_the_band += 1
+        else:
+            above_band += 1
+
+    band_duration_info = {
+        'below_band':round(below_band/percent_value,2),
+        'in_the_band':round(in_the_band/percent_value,2),
+        'above_band':round(above_band/percent_value,2)
+    }
+    return band_duration_info
+
+print(band_duration_info())
