5005214f4eba129d35aa6d45107f6cc6c598b31e
unknown <Administrator@DESKTOP-EE59TLO.wrldc.local>
2019-06-20 17:12:02 +0530

    day wise frequency  band info completed

--- app/frequecny_profile.py
+++ app/frequecny_profile.py
@@ -17,19 +17,17 @@ for row in frequency_cell:
     for col in row:
         frequency_value.append(col.value)
 
-
-max_frequency = round(max(frequency_value),2)
-
-min_frequency = round(min(frequency_value),2)
-
-avg_frequency = round(avg_frequency(frequency_value),2) 
-
-
 def band_duration_info():
     below_band = 0
     in_the_band = 0
     above_band = 0
     percent_value = 86.40
+    max_frequency = round(max(frequency_value),2)
+
+    min_frequency = round(min(frequency_value),2)
+
+    avg_freq = round(avg_frequency(frequency_value),2)
+
     for each in frequency_value:
         if each < 49.90:
             below_band += 1
@@ -37,11 +35,19 @@ def band_duration_info():
             in_the_band += 1
         else:
             above_band += 1
-
+    
+    outside_band = round(below_band/percent_value + above_band/percent_value ,2)
+    hours_outside_band = round( (outside_band/100) * 24 ,2)
     band_duration_info = {
+        'max_freq':max_frequency,
+        'min_freq':min_frequency,
+        'avg_freq':avg_freq,
         'below_band':round(below_band/percent_value,2),
         'in_the_band':round(in_the_band/percent_value,2),
-        'above_band':round(above_band/percent_value,2)
+        'above_band':round(above_band/percent_value,2),
+        'outside_band':outside_band,
+        'hours_outside_band':hours_outside_band,
+        'fdi': round(hours_outside_band/24,2)
     }
     return band_duration_info
 
