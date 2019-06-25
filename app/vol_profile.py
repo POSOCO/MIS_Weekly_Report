345e312dcd1daeb61ce57d45ffd316ae95637be7
unknown <Administrator@DESKTOP-EE59TLO.wrldc.local>
2019-06-25 16:00:56 +0530

    Added handwritten VOl Station XCEL Codes

--- app/vol_profile.py
+++ app/vol_profile.py
@@ -8,13 +8,14 @@ f.write('The code is run at Vol Profile Script '+datetime.now().strftime('%Y-%m-
 
 files = glob.glob("../Files/vol/*.xlsx")
 
-vol_station_list_400KV = {'Bhopal','Khandwa','Itarsi','Damoh','Nagda','Indore','Gwalior','Raipur','Raigarh','Bhilai','Wardha','Dhule','Parli','Boisar','Kalwa','Karad','Asoj','Dehgam','Kasor',
-'Jetpur','Amreli','Vapi'}
-vol_station_list_765KV = {'Sipat','Seoni','Wardh','Bina','Indore','Sasan','Satna','Tamnar','Kotra','Vododara','Durg','Gwalior'}
+vol_station_list_400KV = {'Bhopal':'P','Khandwa':'G','Itarsi':'F','Damoh':'AZ','Nagda':'O','Indore':'N','Gwalior':'AS','Raipur':'BL','Raigarh':'BK','Bhilai':'AQ','Wardha':'AV','Dhule':'AE','Parli':'AC','Boisar':'CA','Kalwa':'V','Karad':'AA','Asoj':'AH','Dehgam':'L','Kasor':'AO',
+'Jetpur':'AJ','Amreli':'BQ','Vapi':'BZ'}
+vol_station_list_765KV = {'Sipat':'AX','Seoni':'AW','Wardh':'BD','Bina':'BC','Indore':'BF','Sasan':'BH','Satna':'BG','Tamnar':'BW','Kotra':'BX','Vododara':'CF','Durg':'BY','Gwalior':'BB'}
 
 
 for file in files:
     wb = load_workbook(filename = file)
     ws = wb.active
     for eachStation in vol_station_list_400KV:
-        vol_cell = ws['C4']
+        vol_cell_min = ws['C4']
+        vol_cell_max = ws['cd']
